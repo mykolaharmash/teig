5d148ea22b107079d352905460eae97842a63571
Nikolay Garmash <garmash.nikolay@gmail.com>
2018-01-12 10:43:39 +0100

    Initial

---
+++ index.js
@@ -0,0 +1,9 @@
+const injectData = require('./lib/inject-data')
+const expandComponents = require('./lib/expand-components')
+const renderTemplate = require('./lib/render-template')
+
+module.exports = {
+  injectData,
+  expandComponents,
+  renderTemplate
+}
